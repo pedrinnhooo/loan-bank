26d920a603e439d9e4d545fd20a17f12d1dd4555
“pedrinnhooo” <pedroruffo0305@gmail.com>
2025-03-13 22:56:56 -0300

    changes/ add styles.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2025-03-10',
+  compatibilityDate: '2025-03-13',
   devtools: { enabled: true },
 
   css: [
     '/node_modules/bootstrap/dist/css/bootstrap.min.css',
-    '/node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
+    '/node_modules/bootstrap-icons/font/bootstrap-icons.min.css',
+    '@/assets/styles/home.css'
   ],
 })
